d1e2577a01618341f29b8412a58f9e4041949cde
Ws-Peroth <a01033056809@gmail.com>
2021-04-13 19:34:51 +0900

    Setting Connect
    Setting Connect

---
+++ Assets/NetworkManage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManage : MonoBehaviourPunCallbacks
+{
+    public string playerName;
+
+    private void Awake()
+    {
+        Screen.SetResolution(960, 540, false);
+        PhotonNetwork.SendRate = 60;
+        PhotonNetwork.SerializationRate = 30;
+    }
+
+    public void Connect() 
+        => PhotonNetwork.ConnectUsingSettings();
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.LocalPlayer.NickName = name;
+        PhotonNetwork.JoinOrCreateRoom
+            ("Room", 
+            new RoomOptions { MaxPlayers = 2}, 
+            null);
+    }
+}
