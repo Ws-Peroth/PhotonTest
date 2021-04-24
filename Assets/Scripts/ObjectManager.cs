cb0cb2ddaf4effa39e008a7106600f5e8cf28af1
Ws-Peroth <a01033056809@gmail.com>
2021-04-25 03:30:55 +0900

    Update ObjectManager.cs

--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -8,5 +8,33 @@ using Photon.Chat;
 
 public class ObjectManager : MonoBehaviour
 {
+    public static ObjectManager objectManager = null;
+    public Queue<GameObject> bulletPool = new Queue<GameObject>();
+
+    private void Start()
+    {
+        if(objectManager == null)
+        {
+            objectManager = this;
+        }
+    }
+
+    public GameObject GetBullet()
+    {
+        GameObject bullet = null;
+
+        if (bulletPool.Count == 0)
+        {
+            bullet = PhotonNetwork.Instantiate(nameof(Bullet), transform.position + new Vector3(SR.flipX ? -0.4f : 0.4f, -0.11f, 0),
+                  Quaternion.identity);
+        }
+        else
+        {
+            bullet = bulletPool.Dequeue();
+        }
+
+        return bullet;
+    }
+
 
 }
